2018462a3eb893f092df388999fb487f00fa6d33
Jerry Satpathy <rutu.satpathy44@gmail.com>
2018-04-25 15:51:05 +0530

    Audio Player Extension
    Started the audio player extension just the basic template will have to add a lot of functionality

---
+++ Scripts/materialize.js
@@ -0,0 +1,7 @@
+var sidenav = document.querySelector(".sidenav");
+var optionsidenav = {
+    edge: "left",
+}
+var SideNavinstance = M.Sidenav.init(sidenav, optionsidenav);
+var parallax=document.querySelector(".parallax");
+var instance = M.Parallax.init(parallax);
