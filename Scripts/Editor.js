16b68c8e96438b98f3d819047be47a459f50e89a
Jerry Satpathy <rutu.satpathy44@gmail.com>
2018-08-16 03:50:15 +0530

    Added a minor chnage

--- Scripts/Editor.js
+++ Scripts/Editor.js
@@ -34,23 +34,20 @@ $(document).ready(() => {
 
 
     $("#File").change((e) => {
-        let Images = new Image()
-        let height = null;
-        let width = null
-
-        Images.onload = function () {
-            height = Images.height;
-            width = Images.width
-        }
         let files = e.target.files[0]
-
+        let Images = new Image()
         let reader = new FileReader();
         reader.onload = function (e) {
             Images.src = e.target.result;
 
-            fileToEdit = e.target.result;
             var name = files.name;
-            $(".custom-file-label").text(name)
+            var patt1=/\.[0-9a-z]+$/i;
+            let extension=name.match(patt1)
+            let nameToDisplay=name.slice(0,7)
+
+
+
+            $(".custom-file-label").text(nameToDisplay+extension)
 
         }
         reader.onerror = function (event) {
@@ -58,6 +55,15 @@ $(document).ready(() => {
         };
         if (files) reader.readAsDataURL(files);
         else alert("Error Loading The Files")
+
+        let height = null;
+        let width = null;
+
+        Images.onload = function () {
+            height = Images.height;
+            width = Images.width
+        }
+
 
 
 
@@ -74,14 +80,82 @@ $(document).ready(() => {
                 orientation: 1,
 
             });
+
+            $("#undoRotate").bind("click", () => {
+                Rotate(editor, {
+                    src: Images.src,
+                    value: 1
+                })
+            })
+            $("#Filter").bind("click", () => {
+                $("#EditorPane").hide();
+                $("#filterTab").show();
+            })
+            $("#Settings").bind("click", () => {
+                $("#EditorPane").hide();
+                $("#settingsPane").show()
+            })
+            $("#closeSettings").bind("click", () => {
+                $("#EditorPane").show();
+                $("#settingsPane").hide()
+            })
+            $("#closeFilter").bind("click", () => {
+                $("#EditorPane").show();
+                $("#filterTab").hide()
+            })
+            //TODO Cannot Initialize Croppy Again Do It Mannualy
+           /* $("#CorpAgain").bind("click", () => {
+                editor.croppie("destroy")
+                $("#EditorPane").hide();
+                let canvas = $("#Retouched").get(0);
+                let newImage = new Image()
+                newImage.src = canvas.toDataURL()
+                let width = null;
+                let height = null;
+                newImage.onload = function () {
+                    width = newImage.width;
+                    height = newImage.height;
+                }
+
+                let Neweditor = Cropper("Retouched", {
+                    width: width,
+                    height: height
+                })
+                Neweditor.croppie('bind', {
+                    url: newImage.src,
+                    orientation: 1,
+
+                });
+                Neweditor.croppie('result', 'base64', "original", "jpeg").then((html) => {
+                    $("#EditorPane").show();
+                    Editor("#Editor", html, function () {
+
+                        this.render();
+
+
+                    })
+                    Editor("#Retouched", html, function () {
+
+                        this.render();
+
+
+                    })
+                })
+
+            })*/
+            $("#Download").bind("click", () => {
+
+            })
+
+            //Will be improved
             $("#Rotate").bind("click", () => {
 
-                let valueToPass=Math.floor(Math.random()*8)
-                if(valueToPass==0)valueToPass==1
+                let valueToPass = Math.floor(Math.random() * 8)
+                if (valueToPass == 0 || valueToPass == 1) valueToPass == 2
                 console.log(valueToPass)
-                Rotate(editor,{
-                    src:Images.src,
-                    value:valueToPass
+                Rotate(editor, {
+                    src: Images.src,
+                    value: valueToPass
                 })
 
             })
@@ -91,7 +165,13 @@ $(document).ready(() => {
                 $("#step3").removeClass("hidden")
 
                 editor.croppie('result', 'base64', "original", "jpeg").then(function (html) {
-                    console.log(html)
+                    editor.
+                    croppie("destroy")
+                    $(".Undo").bind("click", () => {
+                        Editor("#Retouched", html, function () {
+                            this.revert().render()
+                        })
+                    })
                     Editor("#Editor", html, function () {
 
                         this.render();
